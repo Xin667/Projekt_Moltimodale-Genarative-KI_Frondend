2c190986385e04b0da4c80e432484ac33f6eda38
simsek-beyza <beyza.simsek@smail.th-koeln.de>
2026-08-06 13:42:04 +0200

    remove bug

--- draft2device-app/src/api/v2/endpoints/projects/router.py
+++ draft2device-app/src/api/v2/endpoints/projects/router.py
@@ -10,11 +10,22 @@ eigenen Prompt.
 from fastapi import APIRouter
 import db.db as db
 
+import logging
+from fastapi import APIRouter
+
 router = APIRouter()
+
+# Verwendet das vorhandene Uvicorn-Logging
+logger = logging.getLogger("uvicorn.error")
 
 
 @router.post("/projects")
 def create_project_endpoint():
     # Legt ein neues, leeres Projekt an in der DB an und gibt die ID zurück.
     project_id = db.create_project()
+
+
+    logger.info("Neues Projekt erstellt | project_id=%s", project_id)
+
+
     return {"project_id": project_id}
